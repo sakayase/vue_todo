811f2eec17e28ae6ed21b0b37e3452a0dc0719d0
sakayase <sakayase>
2021-02-18 21:54:53 +0100

    todo list delete à finir

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <form v-on:submit="addTask">
+      <input v-model="taskUser">
+      <input type="submit">
+    </form>
+    <div v-if="tasks !== null">
+      <ul v-for="task in tasks" v-bind:key="task.id"> 
+        <Task 
+          v-bind:title="task.title"
+          v-bind:completed="task.completed"
+          v-bind:id="task.id"
+          ></Task>     
+      </ul>
+    </div>
+    <div v-else>
+      Loading...
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Task from './components/Task';
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Task
+  },
+  data () {
+    return {
+      tasks: null,
+      taskUser: '',
+      }
+  },
+  mounted () {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => (this.tasks = response.data))
+  },
+  methods: {
+    addTask: function(event) {
+      this.tasks.push({ id: uuidv4(), title: this.taskUser, completed: false });
+      this.taskUser = ''
+      event.preventDefault();
+    },
   }
 }
 </script>
@@ -24,5 +60,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.completed {
+  color: black;
+  text-decoration: line-through;
+}
+.completed-svg {
+  width:20px;
+}
+.notCompleted {
+  color: black;
+}
+.notCompleted-svg {
+  width: 20px;
+}
+.delete-svg {
+  width: 20px;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+  <li v-if="completed" class="completed">
+    {{ title }}
+    <img
+      class="completed-svg"
+      src="../assets/check-circle-regular.svg"
+      v-on:click="completed = !completed"
+    />
+    <img
+      src="../assets/times-circle-solid.svg"
+      class="delete-svg"
+      @click="removeTask(id)"
+    />
+  </li>
+  <li v-else class="notCompleted">
+    {{ title }}
+    <img
+      class="notCompleted-svg"
+      src="../assets/circle-regular.svg"
+      v-on:click="completed = !completed"
+    />
+    <img
+      src="../assets/times-circle-solid.svg"
+      class="delete-svg"
+      @click="removeTask(id)"
+    />
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    title: String,
+    completed: Boolean,
+    id: Number,
+  },
+  methods: {
+    removeTask: function (id) {
+        console.log(id)
+    },
+  },
+};
+</script>
